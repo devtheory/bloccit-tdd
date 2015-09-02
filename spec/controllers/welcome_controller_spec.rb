1f9b1038dd5ef7f1a70164d4230d24cdd7e12d8a
Jose Sanchez <jparkers0012@gmail.com>
2015-09-02 11:19:53 -0400

    26 testing as

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -26,4 +26,12 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+  describe "GET faq" do
+    it "renders the faq template" do
+      get :faq
+
+      expect(response).to render_template('faq')
+    end
+  end
+
 end
