22fa9241bbfb9aafef5fd879a56e1f2f81a357bb
Jose Sanchez <jparkers0012@gmail.com>
2015-09-03 10:05:02 -0400

    32 crud cp

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: "Post created successfully!"
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
