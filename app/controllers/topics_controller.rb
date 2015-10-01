64a530604d8574b825761e8884729aa5a395e5cf
Jose Sanchez <jparkers0012@gmail.com>
2015-10-01 12:38:01 -0400

    50 private topics cp

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,11 +3,14 @@ class TopicsController < ApplicationController
   before_action :authorize_user, except: [:index, :show]
 
   def index
-    @topics = Topic.all
+    @topics = Topic.visible_to(current_user)
   end
 
   def show
     @topic = Topic.find(params[:id])
+    unless @topic.public || current_user
+      redirect_to new_session_path, notice: "You must be signed in to do that!"
+    end
   end
 
   def new
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -14,21 +14,39 @@ RSpec.describe Topic, type: :model do
   it {should validate_length_of(:name).is_at_least(5)} 
   it {should validate_length_of(:description).is_at_least(15)}
 
-    describe "attributes" do
-      it "should respond to name" do
-        expect(topic).to respond_to(:name)
+  describe "attributes" do
+    it "should respond to name" do
+      expect(topic).to respond_to(:name)
+    end
+
+    it "should respond to description" do
+      expect(topic).to respond_to(:description)
+    end
+
+    it "should respond to public" do
+      expect(topic).to respond_to(:public)
+    end
+
+    it "should be public by default" do
+      expect(topic.public).to be(true)
+    end
+  end
+
+  describe "scopes" do
+    before do
+      @public_topic = create(:topic)
+      @private_topic = create(:topic, public: false)
+    end
+
+    describe "visible_to(user)" do
+      it "returns all topics if user is signed in" do
+        user = User.new
+        expect(Topic.visible_to(user)).to eq([@public_topic, @private_topic])
       end
 
-      it "should respond to description" do
-        expect(topic).to respond_to(:description)
-      end
-
-      it "should respond to public" do
-        expect(topic).to respond_to(:public)
-      end
-
-      it "should be public by default" do
-        expect(topic.public).to be(true)
+      it "returns only public topics if there is no user" do
+        expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
+  end
 end
